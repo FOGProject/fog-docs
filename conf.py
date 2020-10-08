757bfbe1913ec146f09722bc806157c8873fd56b
JJ Fullmer <jfullmer@arrowheaddental.com>
2020-10-08 08:37:26 -0600

    added reference link extension and fixed menu icon list links

--- conf.py
+++ conf.py
@@ -30,8 +30,11 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_rtd_theme']
-
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx.ext.autosectionlabel'
+]
+autosectionlabel_prefix_document = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
