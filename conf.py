04d42cfa32a6a0c435a7ab4936dd53e6e3bd600f
JJ Fullmer <jfullmer@arrowheaddental.com>
2020-10-19 15:39:34 -0600

    revert commented out requirements

--- conf.py
+++ conf.py
@@ -32,9 +32,9 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme',
+    # 'sphinx_rtd_theme',
     'sphinx.ext.autosectionlabel',
-    'pydata_sphinx_theme',
+    # 'pydata_sphinx_theme',
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     # "numpydoc",
