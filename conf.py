7ee5d378139a1713035ffbec511cb0cbcfc9e630
JJ Fullmer <jfullmer@arrowheaddental.com>
2020-10-21 16:51:12 -0600

    try suggested fix for autoscroll

--- conf.py
+++ conf.py
@@ -44,6 +44,7 @@ extensions = [
 ]
 autosummary_generate = True
 autosectionlabel_prefix_document = True
+panels_add_bootstrap_css = False
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
